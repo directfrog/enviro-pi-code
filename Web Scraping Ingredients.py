37441df01010cc9738ec38ac0a8e76855537f7e0
directfrog <directfrog@gmail.com>
2021-02-22 13:56:25 +0000

    Update Web Scraping Ingredients.py

--- Web Scraping Ingredients.py
+++ Web Scraping Ingredients.py
@@ -3,64 +3,75 @@ import re
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-alpha = input("What ingredient do you want to find: ")
-print(" ")
 
-list_RECIPES = []
-list_1 = []
-list_2 = []
-list_3 = []
+class WebScrape:
+  def __init__(self):
+    #alpha = input("What ingredient do you want to find: ")
+    self.alpha = input('enter an ingredient: ')
 
-req = Request("https://www.bbcgoodfood.com/recipes/collection/top-20-winter-recipes") #Or any link (right now only on BBCGoodFood)
-html_page = urlopen(req)
+    self.list_RECIPES = []
+    self.list_1 = []
+    self.list_2 = []
+    self.list_3 = []
+    self.req = Request("https://www.bbcgoodfood.com/recipes/collection/top-20-winter-recipes") #Or any link (right now only on BBCGoodFood)
+    self.html_page = urlopen(self.req)
+    self.soup = BeautifulSoup(self.html_page, "lxml")
+    self.links = []
 
-soup = BeautifulSoup(html_page, "lxml")
+  def scrape(self):
+    for link in self.soup.findAll('a'):
+      self.links.append(link.get('href'))
+    for y in range(len(self.links)):
+       x = re.search("recipes/", str(self.links[y]))
+       z = re.search("https", str(self.links[y]))
+       a = re.search("collection", str(self.links[y]))
+       if x != None and z != None and a == None:
+          self.scrape = scrape_me(self.links[y])
+          z = str(self.scrape.ingredients())
+          x = re.search(self.alpha, z)
+          if x != None:
+            self.list_RECIPES.append(self.links[y])
+            self.title = self.scrape.title()
+            self.instructions = str(self.scrape.instructions())
+            self.list_1.append(z)
+            self.list_2.append(self.title)
+            self.list_3.append(str(self.instructions))
 
-links = []
-for link in soup.findAll('a'):
-  links.append(link.get('href'))
-for y in range(len(links)):
-   x = re.search("recipes/", str(links[y]))
-   z = re.search("https", str(links[y]))
-   a = re.search("collection", str(links[y]))
-   if x != None and z != None and a == None:
-      from recipe_scrapers import scrape_me
-      scrape_me = scrape_me(links[y])
-      z = str(scrape_me.ingredients())
-      x = re.search(alpha, z)
-      if x != None:
-        list_RECIPES.append(links[y])
-        
-        title = scrape_me.title()
-        instructions = str(scrape_me.instructions())
-        list_1.append(z)
-        list_2.append(title)
-        list_3.append(str(instructions))
+  def run(self):
+    for num in range(len(self.list_2)):
+      if num % 2 == 0:
 
-for num in range(len(list_2)):
-  if num % 2 == 0:
+        print(str(num//2+1)+ ". "+self.list_2[num])
 
-    print(str(num//2+1)+ ". "+list_2[num])
+    print(" ")
+    self.choose = int(input("Choose a recipe (number):  "))
 
-print(" ")
-choose = int(input("Choose a recipe (number):  "))
+    print("  ")
+    print("Let's cook",self.list_2[self.choose*2-1])
+    print("")
+    print("Here are the ingredients you need: \n")
 
-print("  ")
-print("Let's cook",list_2[choose*2-1])
-print("")
-print("Here are the ingredients you need: \n")
+    self.ingredients = self.list_1[self.choose*2-1]
+    self.ingredients = self.ingredients.split("'")
 
-ingredients = list_1[choose*2-1]
-ingredients = ingredients.split("'")
+    for x in range(len(self.ingredients)):
+      if x % 2 == 1:
+        print(self.ingredients[x])
 
-for x in range(len(ingredients)):
-  if x % 2 == 1:
-    print(ingredients[x])
+    self.press = input("Type START to continue: ")
 
-press = input("Type START to continue: ")
+    self.instructions = self.list_3[self.choose*2-1]
+    self.instructions = self.instructions.split(".")
+    for x in range(len(self.instructions)):
+      if x % 2 == 1:
+        return self.instructions[x]
+    
 
-instructions = list_3[choose*2-1]
-instructions = instructions.split(".")
-for x in range(len(instructions)):
-  if x % 2 == 1:
-    print(instructions[x])
+
+
+
+
+
+
+
+
